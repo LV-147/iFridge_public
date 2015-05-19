cc65df552a1c7662c65c02b85a9e47e24a0dc8ce
Vladius06 <vl00692@gmail.com>
2015-05-19 19:19:40 +0300

    Vlad's job

--- README.md
+++
@@ -1,1 +0,0 @@
-# iFridge_public
--- iFridge/AppDelegate.m
+++ iFridge/AppDelegate.m
@@ -78,7 +78,7 @@
     if (_managedObjectModel != nil) {
         return _managedObjectModel;
     }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"iFridge" withExtension:@"momd"];
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"iFridge" withExtension:@"mom"];
     _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
     return _managedObjectModel;
 }
