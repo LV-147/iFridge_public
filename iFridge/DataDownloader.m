c943cda0c4858ef7b6947313fd74f0aba29b2421
Vladius06 <vl00692@gmail.com>
2015-05-22 16:55:53 +0300

    Some more core data

---
+++ iFridge/DataDownloader.m
@@ -0,0 +1,13 @@
+//
+//  DataDownloader.m
+//  iFridge
+//
+//  Created by Vladius on 5/21/15.
+//  Copyright (c) 2015 Alexey Pelekh. All rights reserved.
+//
+
+#import "DataDownloader.h"
+
+@implementation DataDownloader
+
+@end
